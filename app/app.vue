06ed9450d4c08f01fc5d98352f288ef26633248d
shopNuxt <65ermek@gmail.com>
2026-06-29 12:56:06 +0200

    запускаем bootsrap

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Добро пожаловать в NUXT-SHOP! 🛒</h1>
+    <p>Магазин на Nuxt 4</p>
+    <NuxtLink to="/catalog">Перейти в каталог</NuxtLink>
   </div>
 </template>
